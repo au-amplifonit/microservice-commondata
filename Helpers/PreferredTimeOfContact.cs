ba9bf365cc0691e40c4c48ca82fe4c322ec4fb48
auamplifon <scott.aquilina@amplifon.com>
2020-01-29 15:43:19 +1100

    initial commit

---
+++ Helpers/PreferredTimeOfContact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPITools.Helpers;
+
+namespace Fox.Microservices.Common.Data.Helpers
+{
+	public enum PreferredTimeOfContact
+	{
+		[EnumValue("02")]
+		[Description("9am-12pm")]
+		Morning,
+
+		[EnumValue("03")]
+		[Description("12pm-3pm")]
+		EarlyAfternoon,
+
+		[EnumValue("04")]
+		[Description("3pm-6pm")]
+		Afternoon,
+
+		[EnumValue("05")]
+		[Description("after 6pm")]
+		Evening,
+
+		[EnumValue("06")]
+		[Description("No Preferences")]
+		NoPreference
+	}
+}
---
+++ Models/Clinician.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPITools.EntityMapper;
+using WebAPITools.Models;
+
+namespace Fox.Microservices.Common.Data.Models
+{
+	public class Clinician: ModelBase
+	{
+		[FieldMapper("EMPLOYEE_CODE")]
+		public string Code { get; set; }
+		[FieldMapper("EMPLOYEE_DESCR")]
+		public string Description { get; set; }
+
+		[FieldMapper("EMPLOYEE_TYPE_CODE")]
+		public string ClinicianTypeCode { get; set; }
+		public string ClinicianTypeDescription { get; set; }
+
+		public override void LoadData<T>(DbContext context, dynamic entity)
+		{
+			base.LoadData<T>(context, (T)entity);
+
+			ClinicianTypeDescription = entity?.SY_EMPLOYEE_TYPE.EMPLOYEE_TYPE_DESCR;
+		}
+	}
+}
